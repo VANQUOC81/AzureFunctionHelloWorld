dc1b86fe04a2b8bce899515268b2a7a567b724c0
Van Quoc Nguyen <quoc.nguyen@vanquoc.com>
2023-09-15 12:50:32 +0200

    add comment to the hostbuilder

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.Extensions.Hosting;
 
+// sets up and runs an Azure Functions worker host using the HostBuilder 
+// and Azure Functions-specific configurations provided by ConfigureFunctionsWorkerDefaults(). 
+// It's the entry point for your Azure Functions application, where the host is started, and your functions are executed in response to events.
+
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .Build();
