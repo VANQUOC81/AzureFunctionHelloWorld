8e5fbd65080dda4425852d16ffe3c25c61e1069d
Van Quoc Nguyen <quoc.nguyen@vanquoc.com>
2023-09-28 10:17:50 +0200

    convert method to async

--- HttpTriggerReturnsData.cs
+++ HttpTriggerReturnsData.cs
@@ -2,6 +2,7 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Common;
 
 namespace Company.Function
 {
@@ -14,15 +15,31 @@ namespace Company.Function
             _logger = loggerFactory.CreateLogger<HttpTriggerReturnsData>();
         }
 
+        // [Function("HttpTriggerReturnsData")]
+        // public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        // {
+        //     _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        //     var response = req.CreateResponse(HttpStatusCode.OK);
+        //     response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        //     response.WriteString("Welcome to Azure Functions HttpTriggerReturnsData!");
+
+        //     return response;
+        // }
+
         [Function("HttpTriggerReturnsData")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public static async Task<HttpResponseData> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        FunctionContext context)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            var logger = context.GetLogger<HttpTriggerReturnsData>();
+            logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions HttpTriggerReturnsData!");
+            await response.WriteStringAsync("Welcome to Azure Functions HttpTriggerReturnsData asynchronously !");
 
             return response;
         }
